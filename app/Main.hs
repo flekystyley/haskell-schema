c207609d3790fb7fa4dbc735e1a5417a9f2bdb32
flekystyley <flekystyley@gmail.com>
2020-08-09 21:24:57 +0900

    added skipMauy;

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,20 @@ module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
--- 型を定義する
 symbol::Parser Char
--- oneOf - 引数にとった文字列が、指定された文字リストかどうか判断し解析します
+-- oneOf - 引数にとった文字列が、指定された文字リストかどうか判断し解析
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
 
 readExpr::String -> String
 
--- parser関数は parse <パーサー> <名前> <パースしたい文字列>
-readExpr input = case parse symbol "lisp" input of
-    Left err -> "No match" ++ show err
-    Right val -> "Found Value"
+-- parse <パーサー> <名前> <パースしたい文字列>
+-- >>(バインド記号)で2つの関数を組み合わせる
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
 
 main :: IO ()
 main = do
