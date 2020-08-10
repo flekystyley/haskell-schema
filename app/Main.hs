dfc3bfe46e00d922600371c0ca7e82573869ef3f
flekystyley <flekystyley@gmail.com>
2020-08-10 20:36:07 +0900

    fix Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,41 @@ module Main where
 
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
 
 symbol::Parser Char
 -- oneOf - 引数にとった文字列が、指定された文字リストかどうか判断し解析
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
-spaces :: Parser ()
+spaces::Parser()
 spaces = skipMany1 space
+
+data LispVal = Atom String
+    | List[LispVal]
+    | DottedList [LispVal] LispVal
+    | Number Integer
+    | String String
+    | Bool Bool
+
+parseString::Parser LispVal
+parseString = do
+        char '"'
+        x <- many (noneOf "\"")
+        char '"'
+        return $String x
+
+parseAtom::Parser LispVal
+parseAtom = do
+        first <- letter <|> symbol
+        rest <- many (letter <|> digit <|> symbol)
+        let atom = first:rest
+        return $case atom of
+            "#t" -> Bool True
+            "#f" -> Bool False
+            _    -> Atom atom
+
+parseNumber::Parser LispVal
+parseNumber = liftM (Number .read) $ many1 digit
 
 readExpr::String -> String
 
